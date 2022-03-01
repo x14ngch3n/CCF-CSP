68ddf812e639e1b391c65e6a5e555432c1114ee2
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-02 00:26:20 +0800

    finish 201509-1

---
+++ 201509-1.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    int count = 0;
+    int prev = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] != prev)
+        {
+            prev = nums[i];
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
