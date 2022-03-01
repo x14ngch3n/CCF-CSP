94397aae3490de0ea8e53e977711671991425658
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-02 00:17:18 +0800

    finish 201503-1

---
+++ 201503-1.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int> > pic(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> pic[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << pic[j][m - 1 - i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
