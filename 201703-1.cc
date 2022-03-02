be40d2afd51a5dfbb3613ac4d314b8dfc5562283
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-02 10:38:56 +0800

    finish 201703-1

---
+++ 201703-1.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, k;
+    cin >> n >> k;
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    int count = 0, weight = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (weight < k)
+        {
+            weight += nums[i];
+        }
+        else
+        {
+            weight = nums[i];
+            count++;
+        }
+    }
+    cout << count + 1 << endl;
+    return 0;
+}
