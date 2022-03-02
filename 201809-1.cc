bac384900daf8535c64742c55e9a9cf4e23ccb09
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-02 14:27:45 +0800

    finish 201809-1

---
+++ 201809-1.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    int firstday[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> firstday[i];
+    }
+    int secondday[n];
+    // implicit type cast
+    secondday[0] = (firstday[0] + firstday[1]) / 2;
+    secondday[n - 1] = (firstday[n - 1] + firstday[n - 2]) / 2;
+    for (int i = 1; i < n - 1; i++)
+    {
+        secondday[i] = (firstday[i - 1] + firstday[i] + firstday[i + 1]) / 3;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << secondday[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
