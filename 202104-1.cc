bb4f267fd66c3f781a1eef6b2ae60635e69e21f0
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-04 09:16:18 +0800

    finish 202104-1

---
+++ 202104-1.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, m, l;
+    cin >> n >> m >> l;
+    int pixels[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> pixels[i][j];
+        }
+    }
+    int *grey = new int[l]();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            grey[pixels[i][j]]++;
+        }
+    }
+    for (int i = 0; i < l; i++)
+    {
+        cout << grey[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
