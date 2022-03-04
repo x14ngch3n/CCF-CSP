75b926bafcdce057a0973ca4b13f30ddad603a93
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-04 09:07:35 +0800

    finish 202012-1

--- 202012-1.cc
+++ 202012-1.cc
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+int ReLU(int n)
+{
+    return max(n, 0);
+}
+
 int main(int argc, char const *argv[])
 {
-    
+    int n;
+    cin >> n;
+    int scores[n][2];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> scores[i][0] >> scores[i][1];
+    }
+    int score = 0;
+    for (int i = 0; i < n; i++)
+    {
+        score += scores[i][0] * scores[i][1];
+    }
+
+    cout << ReLU(score) << endl;
+
     return 0;
 }
