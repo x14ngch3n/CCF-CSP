aacae263ca6ff43e8a8b8399c21cab4ce2e7a09b
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-05 10:27:21 +0800

    update Makefile

--- src/201312-2.cc
+++ src/201312-2.cc
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    string ISBN;
+    getline(cin, ISBN);
+    int ID = 0;
+    int len = ISBN.size();
+    for (int i = 0; i < len - 2; i++)
+    {
+        if (i != 1 && i != 5)
+        {
+            cout << atoi(&ISBN[i]);
+            ID += i * (int)ISBN[i];
+        }
+    }
+    ID = ID % 11;
+    char ID_char = (ID == 10) ? 'X' : to_string(ID)[0];
+    if (ID_char == ISBN[len - 1])
+    {
+        cout << "Right" << endl;
+    }
+    else
+    {
+        ISBN[len - 1] = ID_char;
+        cout << ISBN << endl;
+    }
     return 0;
 }
