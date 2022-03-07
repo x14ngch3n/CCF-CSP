c52dd7a7ad9b93242e5333091260a83e25c60d50
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-07 10:31:25 +0800

    finish 201503-2

---
+++ src/201503-2.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+bool count_num(const pair<int, int> p1, const pair<int, int> p2)
+{
+    if (p1.second != p2.second)
+    {
+        return p1.second > p2.second;
+    }
+    return p1.first < p2.first;
+}
+
+int main(int argc, char const *argv[])
+{
+    int n, tmp;
+    cin >> n;
+    map<int, int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        if (nums.find(tmp) == nums.end())
+        {
+            nums.insert(make_pair(tmp, 1));
+        }
+        else
+        {
+            nums.find(tmp)->second++;
+        }
+    }
+    vector<pair<int, int>> vec;
+    for (map<int, int>::iterator it = nums.begin(); it != nums.end(); it++)
+    {
+        vec.push_back(*it);
+    }
+    sort(vec.begin(), vec.end(), count_num);
+    for (vector<pair<int, int>>::iterator it = vec.begin(); it != vec.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+
+    return 0;
+}
