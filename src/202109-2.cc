6e95ae013b650969e5e1a2096755689929ccf432
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-10 16:11:28 +0800

    finish 202109-2

--- src/202109-2.cc
+++ src/202109-2.cc
@@ -2,48 +2,33 @@
 
 using namespace std;
 
-// O(n^2), cause TLE
-int getNonzeroSeg(int A[], int n, int p)
-{
-    // p-operation
-    for (int i = 0; i < n; i++) {
-        A[i] = (A[i] < p) ? 0 : A[i];
-    }
-    // count all-zero segments
-    int count = 0;
-    int prev = 1;
-    for (int i = 0; i < n; i++) {
-        if (A[i] == 0 && prev != 0) {
-            count++;
-        }
-        prev = A[i];
-    }
-    return count + 1 - (A[0] == 0) - (A[n - 1] == 0);
-}
+#define ADD_DIFF_BYONE(i, j) diffA[i]++, diffA[j + 1]--;
 
+#define N_MAX 1000000
+#define A_MAX 10000
+
+// prefix sum and diffrential
 int main(int argc, char const* argv[])
 {
     int n;
     cin >> n;
-    int A[n];
-    int AMax = 0;
-    // set<int> s;
-    for (int i = 0; i < n; i++) {
+    int A[N_MAX], diffA[A_MAX];
+    A[0] = diffA[0] = 0;
+    for (int i = 1; i <= n; i++) {
         cin >> A[i];
-        // s.insert(A[i]);
-        AMax = max(AMax, A[i]);
     }
-    int maxSeg = 0;
-    // check if has reach max
-    if (getNonzeroSeg(A, n, 1) == (n + 1) / 2) {
-        cout << (n + 1) / 2 << endl;
-    } else {
-        // for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
-        for (int i = 1; i <= AMax; i++) {
-            int seg = getNonzeroSeg(A, n, i);
-            maxSeg = max(seg, maxSeg);
+    for (int i = 1; i <= n; i++) {
+        if (A[i - 1] < A[i]) {
+            ADD_DIFF_BYONE(A[i - 1] + 1, A[i]);
         }
-        cout << maxSeg << endl;
     }
+
+    int maxSeg = 0, prefixSum = 0;
+    for (int i = 1; i < A_MAX; i++) {
+        prefixSum += diffA[i];
+        maxSeg = max(maxSeg, prefixSum);
+    }
+    cout << maxSeg << endl;
+
     return 0;
 }
