f5f720334a49c71e2b280576e202eed6d9cf10da
cascades-sjtu <cascades@sjtu.edu.cn>
2022-03-10 17:05:36 +0800

    add explanation of 202109-2

--- src/202109-2.cc
+++ src/202109-2.cc
@@ -2,30 +2,35 @@
 
 using namespace std;
 
-#define ADD_DIFF_BYONE(i, j) diffA[i]++, diffA[j + 1]--;
+#define ADD_DIFF_BYONE(i, j) diffP[i]++, diffP[j + 1]--;
 
 #define N_MAX 1000000
 #define A_MAX 10000
 
 // prefix sum and diffrential
+// https://www.cnblogs.com/yongcheng137blogs/p/15396599.html
+// https://www.cnblogs.com/yongcheng137blogs/p/15412952.html
 int main(int argc, char const* argv[])
 {
     int n;
     cin >> n;
-    int A[N_MAX], diffA[A_MAX];
-    A[0] = diffA[0] = 0;
+    // diffP is the diffrential sequence of distribution of p
+    int A[N_MAX], diffP[A_MAX];
+    A[0] = diffP[0] = 0;
     for (int i = 1; i <= n; i++) {
         cin >> A[i];
     }
     for (int i = 1; i <= n; i++) {
         if (A[i - 1] < A[i]) {
+            // may cause nonzero segment
             ADD_DIFF_BYONE(A[i - 1] + 1, A[i]);
         }
     }
 
     int maxSeg = 0, prefixSum = 0;
+    // revserse computing diffP(accumulation) to get the P's distribution
     for (int i = 1; i < A_MAX; i++) {
-        prefixSum += diffA[i];
+        prefixSum += diffP[i];
         maxSeg = max(maxSeg, prefixSum);
     }
     cout << maxSeg << endl;
